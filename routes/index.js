4f6a8c4de53f97d0b69119a8e09ebad95554b931
Mmackz <matthewmackenz@gmail.com>
2023-04-01 20:37:21 -0700

    ✨ return json from post routes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const rateLimit = require("express-rate-limit");
 const updateData = require("../lib/get_data");
 
 const limiter = rateLimit({
-   windowMs: 100000, // 1 day
+   windowMs: 300000, // 5 minute
    max: 1, // limit to 1 request per windowMs
    keyGenerator: function (req) {
       // use the same key for all requests
@@ -26,19 +26,19 @@ router.get("/", (req, res, next) => {
    res.render("index");
 });
 
-router.get("/download", (req, res, next) => {
+router.post("/download", (req, res, next) => {
    const filePath = "./lib/get_data/files/output.csv";
    res.download(filePath, "rh_quest_amounts.csv", (err) => {
       if (err) {
          console.error(err);
-         res.status(404).send("File not found");
+         res.status(404).json("File not found");
       }
    });
 });
 
-router.post("/update", async (req, res, next) => {
+router.post("/update", limiter, async (req, res, next) => {
    const updated = await updateData();
-   res.status(200).send(updated);
+   res.status(200).json(updated);
 });
 
 module.exports = router;
