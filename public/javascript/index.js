3c892aae043bf5d65beb57344df71db5ed2fe18a
Mmackz <matthewmackenz@gmail.com>
2023-04-02 20:11:24 -0700

    ✨ setup slider for quest threshold

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,10 +1,21 @@
 const updateButton = document.querySelector("#update");
+const timestamp = document.querySelector(".timestamp");
+
+const slider = document.querySelector("#threshold");
+const thresholdValue = document.querySelector("#threshold-value");
+
+timestamp.textContent = `Last Update: ${new Date(+timestamp.getAttribute("data-timestamp")).toLocaleString()}`;
+
+slider.addEventListener("input", (event) => {
+   thresholdValue.textContent = event.target.value;
+});
 
 updateButton.addEventListener("click", async () => {
    console.log("starting update");
    try {
       const response = await fetch("/update", { method: "POST" });
       const data = await response.json();
+      timestamp.textContent = `Last Update: ${new Date(+data).toLocaleString()}`
       console.log(data);
    } catch (e) {
       console.log(e);
