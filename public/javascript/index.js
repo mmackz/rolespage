ba51f5d61c9f6116b7fd6c2da584e91245995d4f
Mmackz <matthewmackenz@gmail.com>
2023-04-04 19:29:16 -0700

    ✨ show time of last update

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,13 +1,29 @@
+const downloadButton = document.querySelector("#download");
 const updateButton = document.querySelector("#update");
 const timestamp = document.querySelector(".timestamp");
 
 const slider = document.querySelector("#threshold");
 const thresholdValue = document.querySelector("#threshold-value");
 
-timestamp.textContent = `Last Update: ${new Date(+timestamp.getAttribute("data-timestamp")).toLocaleString()}`;
+timestamp.textContent = `Last Update: ${new Date(
+   +timestamp.getAttribute("data-timestamp")
+).toLocaleString()}`;
 
 slider.addEventListener("input", (event) => {
    thresholdValue.textContent = event.target.value;
+});
+
+downloadButton.addEventListener("click", async () => {
+   try {
+      const response = await fetch("/download", {
+         method: "POST",
+         body: JSON.stringify({ threshold: slider.value })
+      });
+      const data = await response.json();
+      console.log(data);
+   } catch (e) {
+      console.log(e);
+   }
 });
 
 updateButton.addEventListener("click", async () => {
@@ -15,8 +31,11 @@ updateButton.addEventListener("click", async () => {
    try {
       const response = await fetch("/update", { method: "POST" });
       const data = await response.json();
-      timestamp.textContent = `Last Update: ${new Date(+data).toLocaleString()}`
-      console.log(data);
+
+      if (response.status === 200) {
+         timestamp.textContent = `Last Update: ${new Date(+data).toLocaleString()}`;
+         console.log(response.status);
+      } else if (response.status === 429) console.log("Too many requests");
    } catch (e) {
       console.log(e);
    }
