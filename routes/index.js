8fdfc5f3710db17e8f6a227ad6190b0fca178d3f
Mmackz <matthewmackenz@gmail.com>
2023-04-08 19:16:36 -0700

    🔧 adjust window for rate limit

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,9 @@ const updateData = require("../lib/get_data");
 const getData = require("../lib/get_data/utils/readFile");
 
 const limiter = rateLimit({
-   windowMs: 300000, // 5 minute
-   max: 1, // limit to 1 request per windowMs
-   keyGenerator: function (req) {
-      // use the same key for all requests
-      return "global";
-   },
+   windowMs: 3_600_000, // 1 hour
+   max: 1,
+   keyGenerator: () => "global",
    handler: function (req, res, next) {
       res.status(429).json("Too many requests");
    }
@@ -33,7 +30,7 @@ router.get("/get_data", async (req, res, next) => {
    );
 });
 
-router.post("/update", limiter, async (req, res, next) => {
+router.get("/update", limiter, async (req, res, next) => {
    const timestamp = await updateData();
    res.status(200).json(timestamp);
 });
