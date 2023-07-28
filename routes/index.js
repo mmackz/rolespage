57aff801965447d1e7bd3429beadbdaeb2cd545e
Mmackz <matthewmackenz@gmail.com>
2023-07-28 12:10:31 -0700

    ✨ add allowlist route

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,32 @@ router.get("/", (req, res, next) => {
    res.render("index", { timestamp, addresses });
 });
 
-router.get("/get_data", async (req, res, next) => {
+router.get("/get_data", async (req, res) => {
    const { results } = getData();
    res.status(200).json(
       Object.entries(results)
          .filter((address) => address[1] >= 10)
          .sort((a, b) => b[1] - a[1])
    );
+});
+
+router.get("/allowlist", async (req, res) => {
+   const { query } = req;
+   const { results } = getData();
+   let threshold = 20;
+   if (query.hasOwnProperty("threshold")) {
+      const param = query.threshold;
+      const paramValue = parseInt(param);
+
+      if (Number.isInteger(paramValue) && paramValue >= 10) {
+         threshold = paramValue;
+      }
+   }
+   const allowlist = Object.entries(results)
+      .filter((address) => address[1] >= threshold)
+      .sort((a, b) => b[1] - a[1])
+      .map((address) => address[0]);
+   res.status(200).json(allowlist);
 });
 
 module.exports = router;
